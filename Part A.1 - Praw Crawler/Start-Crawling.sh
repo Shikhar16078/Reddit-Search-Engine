4f5062d3cc5129ca8f62a95e667ba9b6799c7044
Shikhar Kumar <shikharkumar@ymail.com>
2025-02-14 20:39:23 -0800

    Updated Messages in Mac Shell File

--- Part A.1 - Praw Crawler/Start-Crawling.sh
+++ Part A.1 - Praw Crawler/Start-Crawling.sh
@@ -1,11 +1,5 @@
 read -p "Enter a space-separated sub-reddit topics to crawl: " user_input
 
-if [ -z "$user_input" ]; then
-    echo "No input was provided."
-else
-    echo "These are the inputs: $user_input"
-fi
-
 if [ -n "$user_input" ]; then
     python3 crawl_reddit.py "$user_input"
 else

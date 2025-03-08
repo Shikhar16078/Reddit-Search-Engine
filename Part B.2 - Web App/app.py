2cd216f101991f9b3c90cae53c63c20b6f983268
Shikhar Kumar <shikharkumar@ymail.com>
2025-03-08 01:47:36 -0800

    Major Update: Added URLs to Posts

--- Part B.2 - Web App/app.py
+++ Part B.2 - Web App/app.py
@@ -9,7 +9,7 @@ def search_lucene(query, top_k):
     config_path = r"C:\Users\shikh\Desktop\IR Project\Reddit-Search-Engine\Part B.2 - Web App\utils\config.json"
     
     # Directory containing your LuceneSearch.class file
-    class_dir = r"C:\Users\shikh\Desktop\IR Project\Reddit-Search-Engine\Part B.2 - Web App\utils"
+    class_dir = r"C:\Users\shikh\Desktop\IR Project\Reddit-Search-Engine\Part B.2 - Web App"
     
     lucene_search_class = "LuceneSearch"  # If not in any package
     
@@ -24,6 +24,9 @@ def search_lucene(query, top_k):
         str(top_k),
         query  # Pass the entire query as a single argument
     ]
+    
+    print("Executing command:", " ".join(command)) # Debug
+
     
     try:
         # Run the Java program and capture output
@@ -47,10 +50,8 @@ def index():
         index_type = request.form['index_type']
         
         if index_type == 'lucene':
-            print("Lucene was Searched")
             results = search_lucene(query, top_k)
         elif index_type == 'bert':
-            print("BERT was Searched")
             results = {"message": "BERT search is not implemented yet"}
     
     return render_template('index.html', results=results)
